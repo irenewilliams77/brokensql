851d9f2902c25ba8fe2d3a5c1f9fe3e63ada4cd4
Irene Williams <europeandistribution@ymail.com>
2020-09-09 12:59:44 -0400

    made css changes

--- insert.js
+++ insert.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'brokensql'
+    password: 'root', 
+    database: 'week11'
 });
 
-connection.query('select 1', function (error, result, fields) {
+connection.connect();
+
+connection.query('select * from customer', function (error, result, fields) {
     if(error) throw error;
+    console.log('userinfo', results[0].customer);
 });
+
+connection.end();
